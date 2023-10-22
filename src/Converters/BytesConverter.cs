3ddd759714739ca41d8c3ee3d29f66220f04b78b
Alexander Chalyi <oleksandr.c@hotmail,com>
2023-10-22 10:43:01 +0300

    added BytesConverter

--- src/Converters/BytesConverter.cs
+++ src/Converters/BytesConverter.cs
@@ -10,12 +10,12 @@ internal class BytesConverter : IValueConverter {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
         var byteCount = value is long l ? l : 0;
         if (byteCount == 0) {
-            return "0" + _suffixes[0];
+            return $"0 {_suffixes[0]}";
         }
         var bytes = Math.Abs(byteCount);
         var place = System.Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024)));
         var num = Math.Round(bytes / Math.Pow(1024, place), 1);
-        return (Math.Sign(byteCount) * num).ToString(CultureInfo.InvariantCulture) + _suffixes[place];
+        return $"{(Math.Sign(byteCount) * num).ToString(CultureInfo.InvariantCulture)} {_suffixes[place]}";
     }
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
